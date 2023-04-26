a0324f3564a0aa50c65cd100c3aadb8acc836c9e
edneide.ramalho@gmail.com <edneide.ramalho@gmail.com>
2023-04-26 14:36:40 +0000

    ETL de pagamentos completo

--- src/etl/pagamentos_02.sql
+++ src/etl/pagamentos_02.sql
@@ -1,20 +1,33 @@
 -- Databricks notebook source
-WITH tb_join AS (
-  SELECT t2.*,
-       t3.idVendedor 
 
-  FROM silver.olist.pedido AS t1
+
+-- COMMAND ----------
+
+WITH tb_pedidos AS (
+  SELECT 
+    DISTINCT
+    t1.idPedido,
+    t2.idVendedor
+
+FROM silver.olist.pedido AS t1
+
+LEFT JOIN silver.olist.item_pedido AS t2
+ON t1.idPedido = t2.idPedido
+
+WHERE t1.dtPedido < '2018-01-01'
+AND t1.dtPedido >= add_months('2018-01-01', -6)
+AND t2.idVendedor IS NOT NULL
+),
+
+
+tb_join AS (
+  SELECT t1.idVendedor ,
+         t2.* 
+         
+  FROM tb_pedidos AS t1
 
   LEFT JOIN silver.olist.pagamento_pedido AS t2
   ON t1.idPedido = t2.idPedido
-
-  LEFT JOIN silver.olist.item_pedido AS t3
-  ON t1.idPedido = t3.idPedido
-
-  WHERE dtPedido < '2018-01-01'
-  AND dtPedido >= add_months('2018-01-01', -6)
-  AND t3.idVendedor IS NOT NULL 
-
 ),
 
 tb_group AS (
@@ -26,9 +39,10 @@ tb_group AS (
   FROM tb_join
 
   GROUP BY idVendedor, descTipoPagamento
-)
+),
 
 
+tb_summary AS (
 SELECT idVendedor,
 
 sum(case when descTipoPagamento = 'boleto' then qtdePedidoMeioPagamento else 0 end) as qtde_boleto_pedido,
@@ -51,17 +65,60 @@ sum(case when descTipoPagamento = 'credit_card' then vlPedidoMeioPagamento else 
 sum(case when descTipoPagamento = 'voucher' then vlPedidoMeioPagamento else 0 end)/sum(vlPedidoMeioPagamento) as pct_valor_voucher_pedido ,
 sum(case when descTipoPagamento = 'debit_card' then vlPedidoMeioPagamento else 0 end)/sum(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
 
-
-
 FROM tb_group
 
-GROUP BY 1
+GROUP BY idVendedor
+),
+
+
+
+tb_cartao AS (
+SELECT idVendedor,
+       AVG(nrParcelas) AS avgQtdeParcelas,
+       percentile(nrParcelas, 0.5) AS medianQtdeParcelas,
+       MIN(nrParcelas) AS minQtdeParcelas,
+       MAX(nrParcelas) AS maxQtdeParcelas
+
+FROM tb_join 
+
+WHERE descTipoPagamento = 'credit_card'
+
+GROUP BY idVendedor
+)
+
+
+SELECT 
+      '2018-01-01' as dtReference,
+       t1.*,
+       t2.avgQtdeParcelas,
+       t2.medianQtdeParcelas,
+       t2.minQtdeParcelas,
+       t2.maxQtdeParcelas
+
+
+FROM tb_summary as t1
+
+LEFT JOIN tb_cartao as t2 
+
+ON t1.idVendedor = t2.idVendedor
+
+
 
 -- COMMAND ----------
 
-```SELECT * 
+SELECT 
+  DISTINCT
+  t1.idPedido,
+  t2.idVendedor
 
-FROM silver.olist.itemOM_pedido
+FROM silver.olist.pedido AS t1
+
+LEFT JOIN silver.olist.item_pedido AS t2
+ON t1.idPedido = t2.idPedido
+
+WHERE t1.dtPedido < '2018-01-01'
+AND t1.dtPedido >= add_months('2018-01-01', -6)
+
 
 -- COMMAND ----------
 
